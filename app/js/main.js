aae381a4c849ec49f68173441cd0c90846abe624
Ben Metzger <benjamin_metzger@gensler.com>
2013-02-20 11:54:36 -0600

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,23 @@
+require.config({
+  baseUrl: 'js',
+  paths: {
+    'jquery': '../vendor/jquery/jquery',
+    'underscore': '../vendor/underscore-amd/underscore',
+    'backbone': '../vendor/backbone-amd/backbone',
+    'handlebars': '../vendor/handlebars/handlebars',
+    'async': '../vendor/async/lib/async'
+  }
+});
+
+require(["router"],
+  /**
+   *
+   * Initiates the router for the app.
+   *
+   */
+  function(Router) {
+    if (typeof DEBUG == 'undefined') DEBUG = true;
+    if (typeof PRODUCTION == 'undefined') PRODUCTION = false;
+    return new Router();
+  }
+);
---
+++ app/js/views/app.js
@@ -0,0 +1,33 @@
+define(['backbone', 'underscore', 'jquery'],
+	function (Backbone, _, $) {
+
+  /**
+   * @class App
+   * @extends Backbone.View
+   * The first touch down to the application
+   */
+  var App = Backbone.View.extend({
+    // The tag type of the appview.
+    tagName: "div",
+
+    /**
+     * @method initialize
+     *
+     * This is just the boilerplate so we only print something to the
+     * console.log (WARNING: May not work in IE)
+     */
+    initialize: function() {
+      if(DEBUG){
+        console.log("we're using the debug mode");
+      }
+
+      if(PRODUCTION){
+        console.log("we're using the production mode");
+      }
+      console.log( 'Wahoo!' );
+    }
+
+  });
+
+  return App;
+});
