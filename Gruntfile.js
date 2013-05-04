0c6a4c3626f696017567cadbeb95f8cd18b99c11
BenDavidJamin <benjamindmetzger@gmail.com>
2013-05-04 15:15:47 -0500

    added task to grunt to build and uglify the main.js file

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,11 +13,45 @@ module.exports = function(grunt) {
           document: true
         }
       }
+    },
+
+    requirejs: {
+      compile: {
+        options: {
+          appDir: "app",
+          paths: {
+            'jquery': '../vendor/jquery/jquery',
+            'underscore': '../vendor/underscore-amd/underscore',
+            'backbone': '../vendor/backbone-amd/backbone',
+            'handlebars': '../vendor/handlebars/handlebars',
+            'async': '../vendor/async/lib/async'
+          },
+          dir: "build",
+          name: 'main',
+          mainConfigFile: 'app/js/main.js',
+          optimizeCss: 'standard',
+
+          //How to optimize all the JS files in the build output directory.
+          optimize: "none"
+        }
+      }
+    },
+
+    uglify: {
+      my_target: {
+        files: {
+          'build/js/main.js': ['build/js/main.js']
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
 
   grunt.registerTask('default', ['jshint']);
+
+  grunt.registerTask('build', ['requirejs', 'uglify']);
+
 
 };
